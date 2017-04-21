9e634a7a3a097c2a9b09a03c242aa465f539a669
idrivebears <idrivebears@gmail.com>
2017-04-21 16:20:45 -0500

    afsd

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
-all: procesos procesosVM swap
+all: procesos swap
 
 procesos: procesos.c mmu.o pagefault.o
 	gcc -o procesos procesos.c mmu.o pagefault.o
 
 pagefault.o: pagefault.c mmu.h
 	gcc -c pagefault.c
-
 
 swap:
 	./createswap
--- pagefault.c
+++ pagefault.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mmu.h"
+#include <limits.h>
 
 #define RESIDENTSETSIZE 24
 
@@ -15,6 +16,7 @@ extern struct PROCESSPAGETABLE *ptbr;
 extern struct PROCESSPAGETABLE *gprocesspagetable;
 
 int getfreeframe();
+int swapmemory();
 
 // Rutina de fallos de página
 
@@ -51,16 +53,58 @@ int getfreeframe()
 {
     int i;
     // Busca un marco libre en el sistema
-    for(i=framesbegin;i<systemframetablesize+framesbegin;i++)
+    for(i=framesbegin;i<systemframetablesize+framesbegin;i++) {
         if(!systemframetable[i].assigned)
         {
             systemframetable[i].assigned=1;
             break;
         }
-    if(i<systemframetablesize+framesbegin)
+    }
+
+    if(i<systemframetablesize+framesbegin) {
         systemframetable[i].assigned=1;
-    else
-        i=-1;
-    return(i);
+    }
+
+    else {
+        i = swapmemory();
+    }
+
+    return i;
 }
+
+int swapmemory()
+{
+    // buscar el menos usado
+    int i;
+    unsigned long last = ULONG_MAX;
+    FILE *file_swap;
+    int last_index = -1;
+
+    for (i=0; i<RESIDENTSETSIZE/2; i++)
+    {
+        if(ptbr[i].tlastaccess < last) {
+            last_index = i;
+            last = ptbr[i].tlastaccess;
+        }
+    }
+    printf(">>MENOS USADO: i:%d val:%d\n", last_index, ptbr[last_index]);
+    
+
+    if(ptbr[last_index].modificado == 1) {
+        //escribir a memoria
+        int FN = ptbr[last_index].framenumber;
+        printf(">>Frame fue modificado, escribiendo a memoria.\n");
+        
+        /*file_swap = fopen("swap", "wb");
+        fwrite(&systemframetable[FN],sizeof(systemframetable[FN]),1, file_swap);*/
+    }
+    
+    ptbr[last_index].presente = 0;
+
+
+    printf(">>Swap completado de frame: %d\n", ptbr[last_index].framenumber);
+    return ptbr[last_index].framenumber;
+}
+
+
 
